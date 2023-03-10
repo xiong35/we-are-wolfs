a7a7f142cf45a6613f3b544f73d5b97ecc245295
xiong35 <56919869+xiong35@users.noreply.github.com>
2023-03-10 16:04:34 +0800

    feat(mock): 添加mock包

---
+++ packages/mock/src/players.ts
@@ -0,0 +1,23 @@
+import { IPublicPlayer } from "@werewolf/shared";
+
+export function getMockPlayers(num: number): IPublicPlayer[] {
+  const players = [];
+  for (let i = 0; i < num; i++) {
+    players.push(genPlayer(i));
+  }
+  return players;
+}
+
+function genPlayer(index: number): IPublicPlayer {
+  return {
+    canBeVoted: true,
+    exileVotes: [],
+    index,
+    isAlive: true,
+    isDying: false,
+    isSheriff: true,
+    name: `Player ${index}`,
+    sheriffAssigns: [],
+    sheriffElectVote: 0,
+  };
+}
