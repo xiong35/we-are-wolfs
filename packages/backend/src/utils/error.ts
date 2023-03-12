b185828338e71888a344f6025336e61c549bf1bf
xiong35 <56919869+xiong35@users.noreply.github.com>
2023-03-12 16:45:40 +0800

    feat(be): 添加 httpStatusEnum

--- packages/backend/src/utils/error.ts
+++ packages/backend/src/utils/error.ts
@@ -1,5 +1,7 @@
+import { HttpStatusCode } from "@werewolf/shared";
+
 export type IErrorMsg = {
-  status: number;
+  status: HttpStatusCode;
   msg: string;
 };
 
@@ -7,7 +9,7 @@ export type IErrorMsg = {
  * 接受 status 和 msg，将其包装成 json 字符串传给 Error 的 message
  */
 export class WError extends Error {
-  constructor(status: number, msg: string) {
+  constructor(status: HttpStatusCode, msg: string) {
     super(JSON.stringify({ status, msg }));
   }
 }
