af90b5c6dc8bd2a068e247792bce011884275377
xiong35 <56919869+xiong35@users.noreply.github.com>
2023-03-24 16:41:05 +0800

    feat(be): 完成 LeaveMsgHandler

--- packages/backend/src/handlers/game/charActHandlers/LeaveMsg.ts
+++ packages/backend/src/handlers/game/charActHandlers/LeaveMsg.ts
@@ -1,27 +1,22 @@
+import { Index } from "@werewolf/shared";
 import { Context } from "koa";
 
-import io from "../../..";
-import { GameStatus, TIMEOUT } from "../../../../../werewolf-frontend/shared/GameDefs";
-import { index } from "../../../../../werewolf-frontend/shared/ModelDefs";
-import { Events } from "../../../../../werewolf-frontend/shared/WSEvents";
-import { ChangeStatusMsg } from "../../../../../werewolf-frontend/shared/WSMsg/ChangeStatus";
 import { Player } from "../../../models/PlayerModel";
 import { Room } from "../../../models/RoomModel";
-import { getVoteResult } from "../../../utils/getVoteResult";
-import { GameActHandler, Response, startCurrentState } from "./";
-import { HunterShootHandler } from "./HunterShoot";
+import { WError } from "../../../utils/error";
+import { GameActHandler } from "./";
 
 export const LeaveMsgHandler: GameActHandler = {
-  curStatus: GameStatus.LEAVE_MSG,
+  curStatus: "LEAVE_MSG",
 
-  async handleHttpInTheState(
+  handleHttpInTheState(
     room: Room,
     player: Player,
-    target: index,
+    target: Index,
     ctx: Context
   ) {
     // 结束发言
-    LeaveMsgHandler.endOfState(room);
+    room.gameController.tryEndState("LEAVE_MSG");
 
     return {
       status: 200,
@@ -32,24 +27,29 @@ export const LeaveMsgHandler: GameActHandler = {
 
   startOfState(room) {
     // 此阶段必须有 room.nextStateOfDieCheck, 否则无法进行后续状态
-    if (!room.nextStateOfDieCheck)
-      throw new Error("未设置死亡结算后去到的状态");
+    if (!room.nextStateOfDieCheck) {
+      throw new WError(500, "未设置死亡结算后去到的状态");
+    }
+
     // 玩家死亡后依次进行以下检查
     // 遗言发表检查, 猎人开枪检查, 警长传递警徽检查
-    if (
-      room.currentDay === 1 ||
-      room.nextStateOfDieCheck === GameStatus.WOLF_KILL
-    ) {
+    if (room.currentDay === 1 || room.nextStateOfDieCheck === "WOLF_KILL") {
       // 如果是第一夜或者是放逐投票死的就有遗言
       // 进入留遗言环节
-      startCurrentState(this, room);
+      return {
+        action: "START",
+      };
     } else {
       // 否则无遗言, 结束当前阶段
-      LeaveMsgHandler.endOfState(room);
+      return {
+        action: "END",
+      };
     }
   },
 
-  async endOfState(room) {
-    HunterShootHandler.startOfState(room);
+  endOfState(room) {
+    return {
+      nextState: "HUNTER_SHOOT",
+    };
   },
 };
