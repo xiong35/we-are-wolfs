3d140d646c3b94be70d69f92eedf6ebb11c8c5eb
xiong35 <56919869+xiong35@users.noreply.github.com>
2023-03-24 18:34:43 +0800

    feat(be): 完成 HunterShootHandler

--- packages/backend/src/handlers/game/charActHandlers/HunterShoot.ts
+++ packages/backend/src/handlers/game/charActHandlers/HunterShoot.ts
@@ -1,46 +1,39 @@
+import { IHunterStatus, Index, WSEvents } from "@werewolf/shared";
 import { Context } from "koa";
 
-import io from "../../..";
-import { GameStatus, TIMEOUT } from "../../../../../werewolf-frontend/shared/GameDefs";
-import { index } from "../../../../../werewolf-frontend/shared/ModelDefs";
-import { Events } from "../../../../../werewolf-frontend/shared/WSEvents";
-import { ChangeStatusMsg } from "../../../../../werewolf-frontend/shared/WSMsg/ChangeStatus";
-import { ShowMsg } from "../../../../../werewolf-frontend/shared/WSMsg/ShowMsg";
-import { createError } from "../../../middleware/handleError";
 import { Player } from "../../../models/PlayerModel";
 import { Room } from "../../../models/RoomModel";
-import { getVoteResult } from "../../../utils/getVoteResult";
+import { WError } from "../../../utils/error";
 import { renderHintNPlayers } from "../../../utils/renderHintNPlayers";
-import { GameActHandler, Response, startCurrentState } from "./";
-import { HunterCheckHandler } from "./HunterCheck";
-import { SheriffAssignHandler } from "./SheriffAssign";
+import { emit } from "../../../ws/tsHelper";
+import { GameActHandler } from "./";
 
 export const HunterShootHandler: GameActHandler = {
-  curStatus: GameStatus.HUNTER_SHOOT,
+  curStatus: "HUNTER_SHOOT",
 
-  async handleHttpInTheState(
+  handleHttpInTheState(
     room: Room,
     player: Player,
-    target: index,
+    target: Index,
     ctx: Context
   ) {
     // console.log("# HunterShoot", { player });
     if (player.die?.fromCharacter === "WITCH") {
       // 如果被女巫毒死了就不能开枪
-      createError({
-        msg: "你被女巫毒死, 无法开枪",
-        status: 401,
-      });
+      throw new WError(400, "你被女巫毒死, 无法开枪");
     }
 
-    if (player.characterStatus.shootAt.player > 0)
-      createError({ msg: "你已经开过枪了", status: 401 });
+    const status = player.characterStatus as IHunterStatus;
+    if (status.shootAt.playerIndex > 0) {
+      throw new WError(400, "你已经开过枪了");
+    }
+
+    status.shootAt = {
+      day: room.currentDay,
+      playerIndex: target,
+    };
 
     const targetPlayer = room.getPlayerByIndex(target);
-    player.characterStatus.shootAt = {
-      day: room.currentDay,
-      player: target,
-    };
     targetPlayer.isAlive = false;
     targetPlayer.isDying = true;
     targetPlayer.die = {
@@ -59,40 +52,49 @@ export const HunterShootHandler: GameActHandler = {
   startOfState(room) {
     // 玩家死亡后依次进行以下检查
     // 遗言发表检查, 猎人开枪检查, 警长传递警徽检查
-    if (!showHunter(room)) {
-      // console.log("# HunterShoot", "not show hunter");
-      HunterShootHandler.endOfState(room, false);
+    if (!checkShouldShowHunter(room)) {
+      console.log("# HunterShoot", "not show hunter");
+      return {
+        action: "END",
+        argsToEndOfState: [false],
+      };
     } else {
       // console.log("# HunterShoot", "show hunter");
-      startCurrentState(this, room, true);
+      return {
+        action: "START",
+        argsToEndOfState: [true],
+      };
     }
   },
 
-  async endOfState(room, showHunter: boolean) {
-    if (!showHunter) {
+  endOfState(room, shouldShowHunter: boolean) {
+    if (!shouldShowHunter) {
       // 无猎人? 直接取消这两个阶段
       // console.log("# HunterShoot", "really not show hunter");
-      return SheriffAssignHandler.startOfState(room);
+      return {
+        nextState: "SHERIFF_ASSIGN",
+      };
     }
 
     const shotByHunter = room.players.find(
       (p) => p.die?.fromCharacter === "HUNTER"
     );
+
     if (!shotByHunter) {
       // 到点了未选择则不进行操作, 直接进入警长传警徽阶段, 或者无猎人
-      io.to(room.roomNumber).emit(Events.SHOW_MSG, {
-        innerHTML: "死者不是猎人或选择不开枪",
-      } as ShowMsg);
-      HunterCheckHandler.startOfState(room);
+      emit(room.roomNumber, WSEvents.SHOW_MSG, {
+        innerHTML: "死者不是猎人或选择不开枪或无法开枪",
+      });
     } else {
       // 如果死人了, 通知死人了
-      io.to(room.roomNumber).emit(Events.SHOW_MSG, {
-        innerHTML: renderHintNPlayers("猎人开枪射杀了", [
-          shotByHunter.index,
-        ]),
-      } as ShowMsg);
-      HunterCheckHandler.startOfState(room);
+      emit(room.roomNumber, WSEvents.SHOW_MSG, {
+        innerHTML: renderHintNPlayers("猎人开枪射杀了", [shotByHunter.index]),
+      });
     }
+
+    return {
+      nextState: "HUNTER_CHECK",
+    };
   },
 };
 
@@ -100,20 +102,19 @@ export const HunterShootHandler: GameActHandler = {
  * 是否需要让大家等猎人开枪
  * 如果猎人开过枪或者无猎人就不需要进行此阶段了
  */
-function showHunter(room: Room): boolean {
+function checkShouldShowHunter(room: Room): boolean {
   // console.log("# HunterShoot", { room });
   if (!room.needingCharacters.includes("HUNTER")) return false;
 
-  const hunter = room.players.find(
-    (p) => p.character === "HUNTER"
-  );
+  const hunter = room.players.find((p) => p.character === "HUNTER");
 
   // console.log("# HunterShoot", {
   //   hunter: hunter?.characterStatus?.shootAt,
   // });
   if (!hunter) return false;
 
-  if (hunter.characterStatus?.shootAt?.player > 0) return false;
+  const { shootAt } = hunter.characterStatus as IHunterStatus;
+  if (shootAt.playerIndex > 0) return false;
 
   return true;
 }
