8f1befebaac2bdb46f728b0984590ef665bafac5
xiong35 <xiong35@qq.com>
2023-03-28 14:51:24 +0800

    feat(fe): 完成 ws - gameBegin

---
+++ packages/frontend/src/api/ws/gameBegin.ts
@@ -0,0 +1,13 @@
+import { WSEvents } from "@werewolf/shared";
+import { router } from "../../router";
+import { showDialog } from "../../signals/dialog";
+import { HandlerOf } from "./tsHelper";
+
+export const gameBegin: HandlerOf<WSEvents.GAME_BEGIN> = () => {
+  console.log("#ws on game begin");
+
+  showDialog("游戏开始, 天黑请闭眼👁️");
+  setTimeout(() => {
+    router.navigate("/play");
+  }, 500);
+};
